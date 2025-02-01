edce43892e144efa30d07ae22ae42d9a3023136c
Libanad <libanadhikari33@gmail.com>
2025-02-02 00:31:06 +0545

    done

--- service/sendOtp.js
+++ service/sendOtp.js
@@ -19,7 +19,7 @@ const sendOtp = async (phone, otp) => {
         }
 
     }catch(error){
-        console.log('Error Sending OTP',error.message);
+        console.log('Error Sending OTP',error.message);// otp send
     }
 
     return isSent;
